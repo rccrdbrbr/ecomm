2887bd69502d751a22a3fec6ed4e7b2e2dcf4714
rccrdbrbr <63239705+rccrdbrbr@users.noreply.github.com>
2021-02-27 08:56:24 +0100

    Fix piccolo problema

--- backend/create-ad.inc.php
+++ backend/create-ad.inc.php
@@ -35,17 +35,17 @@ if (isset($_POST["submit"])) {
     }
 
     if (AreaErrata($visibility, $area)!== false) {
-        header("location: ../create-ad.php?error=wronginput1");
+        header("location: ../create-ad.php?error=wronginput");
         exit();
     }
 
     if (AssicurazioneErr($type, $ensurance)!== false) {
-        header("location: ../create-ad.php?error=wronginput2");
+        header("location: ../create-ad.php?error=wronginput");
         exit();
     }
 
     if (UsuraPeriodoErr($type, $usura, $periodo)!== false) {
-        header("location: ../create-ad.php?error=wronginput3");
+        header("location: ../create-ad.php?error=wronginput");
         exit();
     }
 
